076e65b1ce8d3a10ac784f3ae74493ce23170a99
ThrawnsRevenge <ThrawnsRevenge@gmail.com>
2015-07-27 02:26:28 -0400

    PA Survival

---
+++ Data/Scripts/Story/Survival_GC_Script_Pentastar.lua
@@ -0,0 +1,48 @@
+require("PGStateMachine")
+require("PGStoryMode")
+
+
+
+
+function Definitions()
+
+	DebugMessage("%s -- In Definitions", tostring(Script))
+	StoryModeEvents = { 
+				Universal_Story_Start = Begin_GC,
+				STORY_FLAG_Battle_Over = Get_WaveCount }
+
+
+
+
+end
+
+
+
+function Begin_GC(message)
+	if message == OnEnter then
+		GlobalValue.Set("Survival_Mode", 1)
+	end
+end
+
+
+
+function Get_WaveCount(message)
+	if message == OnEnter then
+		
+		wave_count = GlobalValue.Get("WaveCount") -1
+		
+		--wave_count = 5
+		plot = Get_Story_Plot("Story_Sandbox_Survival_Pentastar.xml")
+		event = plot.Get_Event("Show_Wave_Dialog")
+		dialog = "Dialog_Survival"
+
+
+		event.Set_Dialog(dialog)
+		event.Clear_Dialog_Text()
+		--event.Add_Dialog_Text("TEXT_UNIT_VENGEANCE_FRIGATE")
+		event.Add_Dialog_Text("TEXT_WAVES_SURVIVED", wave_count)
+		Story_Event("DIALOG")
+
+
+	end
+end
